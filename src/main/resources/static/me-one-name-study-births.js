52e672fb53ca1eaa4c147507ccb43e5c4baef2ea
metherton <martinetherton63@gmail.com>
2018-10-19 20:12:57 +0200

    added births

---
+++ src/main/resources/static/me-one-name-study-births.js
@@ -0,0 +1,67 @@
+
+class MeOneNameStudyBirths extends HTMLElement {
+
+  constructor() {
+    super();
+    this._root = this.attachShadow({"mode": "open"});
+    this._$persons = null;
+  }
+
+  connectedCallback() {
+    this._root.innerHTML = `
+      <style>
+        p {
+          color: red;
+        }
+      </style>
+      <p>Me One Name Study Births</p>
+      <section id="persons"></section>
+    `;
+
+    this._$persons = this._root.querySelector('#persons');
+
+
+
+    // fetch(`http://www.martinetherton.com:9000/users`)
+    //   .then((response) => response.text())
+    //   .then((responseText) => {
+    //     this.render(JSON.parse(responseText));
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+
+  }
+
+  render(userData) {
+
+    for (let i = 0; i < userData.length; i += 1) {
+      const user = document.createElement('p');
+      user.innerText = userData[i].firstName;
+      this._$persons.appendChild(user);
+    }
+    // this._$persons.innerText = 'hello';
+  }
+
+  renderBirths() {
+    for (let i = 0; i < this._users.length; i += 1) {
+      const user = document.createElement('p');
+      user.innerText = this._users[i].firstName;
+      this._$persons.appendChild(user);
+    }
+  }
+
+  // use setters and getters to create an API for the component
+  set births(data) {
+    if (this._births === data) return;
+    this._births = data;
+    this.renderBirths();
+  }
+
+  get births() {
+    return this._births;
+  }
+
+}
+
+window.customElements.define('me-one-name-study', MeOneNameStudy);
--- src/main/resources/static/me-one-name-study.js
+++ src/main/resources/static/me-one-name-study.js
@@ -4,7 +4,8 @@ class MeOneNameStudy extends HTMLElement {
   constructor() {
     super();
     this._root = this.attachShadow({"mode": "open"});
-    this._$persons = null;
+    this._$birthsButton = null;
+  //  this._$persons = null;
   }
 
   connectedCallback() {
@@ -14,37 +15,47 @@ class MeOneNameStudy extends HTMLElement {
           color: red;
         }
       </style>
+      <script src="me-one-name-study-births.js"></script>
       <p>Me One Name Study</p>
-      <section id="persons"></section>
+      <button id="births">Show Births</button>
+      <me-one-name-study-births id="births"></me-one-name-study-births>
+      <!--<section id="persons"></section>-->
     `;
 
-    this._$persons = this._root.querySelector('#persons');
+    // this._$persons = this._root.querySelector('#persons');
 
 
 
-    fetch(`http://www.martinetherton.com:9000/users`)
-      .then((response) => response.text())
-      .then((responseText) => {
-        this.render(JSON.parse(responseText));
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    // fetch(`http://www.martinetherton.com:9000/users`)
+    //   .then((response) => response.text())
+    //   .then((responseText) => {
+    //     this.render(JSON.parse(responseText));
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+    
+    this._$birthsButton = this._root.querySelector('#births');
+    this._$birthsButton.addEventListener('click', () => {
+      fetch(`births.json`)
+        .then((response) => response.text())
+        .then((responseText) => {
+          this.setBirths(JSON.parse(responseText));
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      
+    });
 
   }
 
+  setBirths(births) {
+    const $births = document.getElementById("births");
+    $ons.births = births;
+  }
+  
   render(userData) {
-    // Fill the respective areas of the card using DOM manipulation APIs
-    // All of our components elements reside under shadow dom. So we created a this.shadowRoot property
-    // We use this property to call selectors so that the DOM is searched only under this subtree
-    // this.shadowRoot.querySelector('.card__full-name').innerHTML = userData.name;
-    // this.shadowRoot.querySelector('.card__user-name').innerHTML = userData.username;
-    // this.shadowRoot.querySelector('.card__website').innerHTML = userData.website;
-    // this.shadowRoot.querySelector('.card__address').innerHTML = `<h4>Address</h4>
-    //   ${userData.address.suite}, <br />
-    //   ${userData.address.street},<br />
-    //   ${userData.address.city},<br />
-    //   Zipcode: ${userData.address.zipcode}`
 
     for (let i = 0; i < userData.length; i += 1) {
       const user = document.createElement('p');
@@ -54,7 +65,24 @@ class MeOneNameStudy extends HTMLElement {
     // this._$persons.innerText = 'hello';
   }
 
+  // renderUsers() {
+  //   for (let i = 0; i < this._users.length; i += 1) {
+  //     const user = document.createElement('p');
+  //     user.innerText = this._users[i].firstName;
+  //     this._$persons.appendChild(user);
+  //   }
+  // }
 
+  // use setters and getters to create an API for the component
+  // set users(data) {
+  //   if (this._users === data) return;
+  //   this._users = data;
+  //   this.renderUsers();
+  // }
+  //
+  // get users() {
+  //   return this._users;
+  // }
 
 }
 
