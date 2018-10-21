4cd51ad4084aeaaf6239b78edc07b8a65da07840
metherton <martinetherton63@gmail.com>
2018-10-21 21:37:38 +0200

    added births

--- src/main/resources/static/me-biography.js
+++ src/main/resources/static/me-biography.js
@@ -1,15 +1,23 @@
 class MeBiography extends HTMLElement {
   constructor() {
     super();
+    this.awesomest = null;
   }
   connectedCallback() {
+    this.awesomest = true;
     this.innerHTML = `
       <style>
         p {
           color: red;
         }
+        .martin::slotted(h1) {
+          color: green;
+        }
       </style>
-      <p>Me Biography</p>
+      <div class="martin">
+        <slot name="bla">Me default Biography</slot>      
+        <slot>should be replaced</slot>
+      </div>
     `;
 
   }
