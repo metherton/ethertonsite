8243c79c9f0bf3be0b773984e57c93b5c2c5bf84
metherton <martinetherton63@gmail.com>
2018-11-01 20:41:31 +0100

    fixed e2e getting title

--- tests/example.js
+++ tests/example.js
@@ -2,6 +2,14 @@ module.exports = {
   'Demo test' : function (browser) {
     browser
       .url(browser.launchUrl)
-      .end();
+      .waitForElementPresent('title', 1000)
+      .pause(1000);
+
+    browser.getTitle(function(title) {
+      this.assert.ok(title.includes('martinetherton.com Home Page'));
+    });
+
+
+    browser.end();
   }
 };
